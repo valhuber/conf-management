169272801a411743fde39ca1812d056dd58fe7ce
Val Huber <val.huber@broadcom.com>
2019-05-28 10:42:15 -0700

    Minimally runnning pull timer
    Though, oddly need to past arrays, not rows...

--- timers/PullOffersTimer.js
+++ timers/PullOffersTimer.js
@@ -17,33 +17,26 @@ TODO - since last-run
 
 try {
     var pullConfOfferURL = Config.settings.confOfferURL + "/ManagementAlert";
-    var getParams = {sysfilter: "like(\"name\":" + JSON.stringify("%Pull Not Push%") + ")"};
-    pulledOffers = timerUtil.restGet(pullConfOfferURL , getParams, Config.settings.confOfferAuthHeader);
-    print("\n" + title + "Wake-up, found: " + pulledOffers);
+    var getParams = {sysfilter: "like(\"name\":" + JSON.stringify("%Pull Not Push%") + ")"};  // TODO and approved
+    pulledOffersResponse = timerUtil.restGet(pullConfOfferURL , getParams, Config.settings.confOfferAuthHeader);
+    print("\n" + title + "Wake-up, found: " + pulledOffersResponse);
 } catch (e) {
     print("\n" + title + "EXCEPTION on wakeup: " + e);
 }
 
-var qItems = JSON.parse(qItemsResponse);  
-for each (var eachQItem in qItems) {
-    print(title + "eachQItem: " + JSON.stringify(eachQItem));
-    eachQItem.ProcessedStatus = "ASYNC - CLAIM";
-    print(title + "setInProgress: " + JSON.stringify(eachQItem));
-    var setInProgressResponse = timerUtil.restPut(timerWakeupSystemQueueURL,
-                                            {}, Config.settings.authHeader, JSON.stringify(eachQItem));
-    print(title + "setInProgressResponse: " + setInProgressResponse);
-    var unclaimed = true;  // TODO - check for optLock exception (another cluster node claimed it)
-    if (unclaimed) {
-        try {    // persist payload using function: http://localhost:8080/rest/default/conf-management/v1/"main:SystemQueue"/[pk]/RetryPayload
-            var timerWakeupProcessingSystemQueueURL = timerWakeupSystemQueueURL + 
-                "/" + eachQItem.Ident + "/RetryPayload";
-            print(title + "ASYNC - PROCESSING.. URL: " + timerWakeupProcessingSystemQueueURL);
-            var asyncProcessResponse = timerUtil.restGet(timerWakeupProcessingSystemQueueURL, {}, Config.settings.authHeader);
-            print(title + "ASYNC - PROCESSING.. asyncProcessResponse: " + asyncProcessResponse + "\n\n");
-        } catch(e) {
-            print (title + "post exception: " + e);
-            throw title + "post exception: " + e;
-        }
-        // TODO - update Q with status
+var pulledOffers = JSON.parse(pulledOffersResponse); 
+var postArrays = [];
+for each (var eachPulledOffer in pulledOffers) {
+    delete eachPulledOffer["@metadata"];
+    print(title + "eachPulledOffer: " + JSON.stringify(eachPulledOffer));
+    postArrays = [];
+    try {    // we have read data in POSTable format... do so!
+        postArrays.push(eachPulledOffer);
+        var postPayloadResponse = timerUtil.restPost(Config.settings.resourceURL + "/ProcessChargesResource",
+                                                {}, Config.settings.authHeader, postArrays);
+        print(title + "Payload POSTed to ProcessChargesResource (queued for async processing), postPayloadResponse: " + postPayloadResponse + "\n\n");
+    } catch(e) {
+        print (title + "post exception: " + e);
+        throw title + "post exception: " + e;
     }
 }
